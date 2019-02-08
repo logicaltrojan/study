f00648f04446b590242b8ff3a3f85a2481f3620d
조경대 <2012027345@hanyang.ac.kr>
2019-02-09 00:58:37 +0900

    public directory

---
+++ jkd4242/20190207/test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int N;
+
+    scanf("%d", &N);
+
+    while(N--){
+
+        char c;
+        while(scanf("%c ", &c) && c != '\n'){
+            printf("%c", c);
+        }
+
+    }
+    
+    
+}
